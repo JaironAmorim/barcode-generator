cd864aef63b587d2c925df858edaa0de85e73570
Jairon Amorim <jairon.amorim@grupomateus.com.br>
2022-09-08 23:05:23 -0300

    A-Primeiro Commit

---
+++ src/main/java/com/example/codegenerator/BarcodesController.java
@@ -0,0 +1,39 @@
+package com.example.codegenerator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.image.BufferedImage;
+
+@RestController
+@RequestMapping("/barcodes")
+public class BarcodesController {
+
+    @GetMapping(value = "/zxing/ean13/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> zXingEAN13Barcode(@PathVariable("barcode") String barcode){
+        return okResponse(CodeGenerator.generateEAN13BarCodeImg(barcode));
+    }
+
+    @GetMapping(value = "/zxing/qrcode/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> zXingQRBarcode(@PathVariable("barcode") String barcode) throws Exception {
+        return okResponse(CodeGenerator.generateQRCodeImg(barcode));
+    }
+    private ResponseEntity<BufferedImage> okResponse(BufferedImage img) {
+        return new ResponseEntity<>(img, HttpStatus.OK);
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttMessageConverter(){
+        return new BufferedImageHttpMessageConverter();
+    }
+
+
+}
---
+++ src/main/java/com/example/codegenerator/CodeGenerator.java
@@ -0,0 +1,24 @@
+package com.example.codegenerator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+
+public class CodeGenerator {
+
+    public static BufferedImage generateEAN13BarCodeImg(String content){
+        EAN13Writer barcodeWriter = new EAN13Writer();
+        BitMatrix bitMatrix = barcodeWriter.encode(content, BarcodeFormat.EAN_13, 300, 150);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+    public static BufferedImage generateQRCodeImg(String content) throws Exception{
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(content, BarcodeFormat.QR_CODE, 200, 200);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+}
